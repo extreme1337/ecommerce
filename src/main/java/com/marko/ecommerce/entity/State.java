4d2709acc426dce1960c7bb29ef731b38e2a5073
Marko Miseljic <marko.miseljic.14@gmail.com>
2020-07-21 13:01:56 +0200

    States and Countries
    Added backend for states and countries

---
+++ src/main/java/com/marko/ecommerce/entity/State.java
@@ -0,0 +1,21 @@
+package com.marko.ecommerce.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "state")
+@Getter
+@Setter
+public class State {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(name = "name")
+    private String name;
+    @ManyToOne
+    @JoinColumn(name = "country_id")
+    private Country country;
+}
