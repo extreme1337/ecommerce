a76cb72f5a9e8d8b78a9af874cc01f1ffa631381
Marko Miseljic <marko.miseljic.14@gmail.com>
2021-02-05 12:51:11 +0100

    backend refactoring

--- src/main/java/com/marko/ecommerce/config/MyDataRestConfig.java
+++ src/main/java/com/marko/ecommerce/config/MyDataRestConfig.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 import javax.persistence.EntityManager;
 import javax.persistence.metamodel.EntityType;
@@ -27,7 +28,7 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
     }
 
     @Override
-    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
 
         HttpMethod[] theUnsupportedActions = {HttpMethod.PUT,HttpMethod.POST,HttpMethod.DELETE};
 
@@ -43,10 +44,10 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
         //disable HTTP methods for State: PUT,POST and DELETE
         disableHttpMethods(State.class,config, theUnsupportedActions);
 
-
-
         //call an internal helper method
         exposeIds(config);
+
+        cors.addMapping("/api/**").allowedOrigins("http://localhost:4200");
     }
 
     private void disableHttpMethods(Class theClass,RepositoryRestConfiguration config, HttpMethod[] theUnsupportedActions) {
